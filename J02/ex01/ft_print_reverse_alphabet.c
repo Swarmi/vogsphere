2024947def0cbfb1f50468a4ab2e1fb9fded80d8
Saad <Sadda.benhalima@gmail.com>
2017-08-14 20:24:09 +0200

    ex02 terminee

---
+++ J02/ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_print_reverse_alphabet(void)
+
+{
+	int a;
+	a='z';
+
+	while ( a >='a') 
+        {
+		write (1, &a, 1);
+		a--;
+	}
+}
+int	main()
+{
+	ft_print_reverse_alphabet();
+	write(1, "\n", 1);
+	return (0);
+}
